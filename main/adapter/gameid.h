27ee85d0de2371d7435bf34d03ef2d0134040e0c
Odlanier-Lopez <yayixbox@gmail.com>
2026-08-21 02:11:22 -0400

    Add SD info update and retrieval functions

--- main/adapter/gameid.h
+++ main/adapter/gameid.h
@@ -12,4 +12,8 @@ int32_t gid_update(struct raw_fb *fb_data);
 int32_t gid_update_sys(struct raw_fb *fb_data);
 char *gid_get(void);
 
+int32_t sd_info_update(struct raw_fb *fb_data);
+uint32_t sd_info_get_total(void);
+uint32_t sd_info_get_free(void);
+
 #endif /* _GAMEID_H_ */
